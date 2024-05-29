8cbbd8f6e8f081ab4ea44e36fa03fd911f27960c
BuiThiThuyTrang <168885695+BuiThiThuyTrang@users.noreply.github.com>
2024-05-29 14:27:36 +0700

    Add files via upload
    Lớp setting

--- Settings.java
+++ Settings.java
@@ -1,38 +1,25 @@
-package main.gui;
-
-import main.game.Player;
+package main.game;
 
 public class Settings {
-
-	// cấu hình và cài đặt chung cho trò chơi
 	
-	public static Colour AIcolour = Colour.BLACK; 
-    
-	  
-    public static boolean helpMode = true;
-    public static boolean hintMode = false;
-    public static boolean dragDrop = false;
-    
-    public static int squareSize = 80;
- 
-    public static int checkerWidth = 5*squareSize/6;
-    public static int checkerHeight = 5*squareSize/6;
- 
-    public static int ghostButtonWidth = 30*squareSize/29;
-    public static int ghostButtonHeight = 5*squareSize/6;
+	
+	public static boolean FORCETAKES = true;
+	
 
- //trả về màu sắc của người chơi dựa trên đối tượng Player.
-    public static Colour getColour(Player player){
-        Colour result = null;
-        if (player == Player.AI){
-            result = Settings.AIcolour;
-        }
-        else if (player == Player.HUMAN){
-            result = Settings.AIcolour.getOpposite();
-        }
-        if(result == null){
-            throw new RuntimeException("Người chơi không có quân cờ.");
-        }
-        return result;
-    }
+	public static Player FIRSTMOVE = Player.HUMAN;
+
+	
+	
+	public static int AI_DEPTH = 7;
+
+	
+	public static final int UNDO_MEMORY = 20;
+
+
+	public static String SELECTED_ALGORITHM = "minimax";
+	
+	public static String SELECTED_ALGORITHM1 = "alphabeta";
+
+	
+	public static int HEURISTIC = 1;
 }
