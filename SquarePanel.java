1ab398b581f1d6f1e0d21cdbf8bd42025327b317
VuTan <106821592+VuTan@users.noreply.github.com>
2024-05-29 14:25:23 +0700

    add
    123123

---
+++ SquarePanel.java
@@ -0,0 +1,45 @@
+package main.gui;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class SquarePanel extends JPanel {
+
+	  
+    private Color color;
+
+    public SquarePanel(int i, int j){
+    	
+    	
+        this.setPreferredSize(new Dimension(Settings.squareSize,Settings.squareSize));
+        
+       
+        if( ((i % 2) + (j % 2)) % 2 == 0){
+            color = hexToColor("#9E6B55");
+        }
+        else{
+            color = hexToColor("#F5C9B2");
+        }
+    }
+
+    public void setHighlighted(){
+        color = hexToColor("#E6B655");
+    }
+
+   
+    private static Color hexToColor(String hex) {
+        if (hex.startsWith("#")) {
+            hex = hex.substring(1);
+        }
+        int rgb = Integer.parseInt(hex, 16);
+        return new Color(rgb);
+    }
+    
+    
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        g.setColor(color);
+        g.fillRect(0, 0, getWidth(), getHeight());
+    }
+
+}
