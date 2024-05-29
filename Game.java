d8f87f85edbedcc6b0e9b57a031f3a204f77aab0
Phan Thi Thuy Duong <21130331@st.hcmuaf.edu.vn>
2024-05-29 14:26:25 +0700

    Add files via upload
    them cach di chuyen

--- Game.java
+++ Game.java
@@ -9,28 +9,24 @@ public class Game {
 
 	private int memory;
 
-	private AI ai;
-
 	private boolean humanWon;
 
 	public Game() {
 		memory = Settings.UNDO_MEMORY;
 		state = new Stack<>();
 		state.push(BoardState.InitialState());
-		ai = new AI();
 	}
 
-	//Cập nhật trạng thái của bàn cờ nếu trò chơi chưa kết thúc và đến lượt người chơi.
+	// thực hiện nước đi của người chơi
 	public void playerMove(BoardState newState) {
 
-		if (!isGameOver() && state.peek().getTurn() == Player.HUMAN) {
-
+		if (!isGameOver() && state.peek().getTurn() == Player.HUMAN
+				|| (!isGameOver() && state.peek().getTurn() == Player.AI)) {
+// cập nhật bằng trạng thái mới
 			updateState(newState);
 		}
 	}
-	
-	//Xử lý nước đi của người chơi từ vị trí fromPos đến vị trí toPos với dịch chuyển dx và dy.
-	//Kiểm tra và trả về các phản hồi khác nhau dựa trên tính hợp lệ của nước đi (ví dụ: không đi ra khỏi bàn cờ, không di chuyển chéo không hợp lệ, ô đến không trống, v.v.).
+
 	public MoveFeedback playerMove(int fromPos, int dx, int dy) {
 
 		int toPos = fromPos + dx + BoardState.SIDE_LENGTH * dy;
@@ -78,8 +74,7 @@ public class Game {
 
 		return MoveFeedback.UNKNOWN_INVALID;
 	}
-	
-	//Trả về phản hồi dựa trên trạng thái hiện tại và các nước đi nhảy có thể.
+
 	public MoveFeedback moveFeedbackClick(int pos) {
 
 		ArrayList<BoardState> jumpSuccessors = this.state.peek().getSuccessors(true);
@@ -91,35 +86,13 @@ public class Game {
 		}
 
 	}
-	
-	//Trả về danh sách các trạng thái kế tiếp hợp lệ cho một vị trí cụ thể.
+// ds trạng thái bàn cờ sau các bước đi hợp lệ.
 	public ArrayList<BoardState> getValidMoves(int pos) {
-
+//lấy ra trạng thái hiện tại của bàn cờ sau khi thực hiện các bước đi hợp lệ 
 		return state.peek().getSuccessors(pos);
 	}
 
-	public void aiMove() {
-
-		if (!isGameOver() && state.peek().getTurn() == Player.AI) {
-			String selectedAlgorithm = Settings.SELECTED_ALGORITHM;
-
-			if ("minimax".equals(selectedAlgorithm)) {
-				BoardState newState = ai.move(this.state.peek(), Player.AI, selectedAlgorithm);
-				updateState(newState);
-			}else if ("alphabeta".equals(Settings.SELECTED_ALGORITHM1)) {
-					BoardState newState = ai.move(this.state.peek(), Player.AI, selectedAlgorithm);
-					updateState(newState);
-			}
-								
-			} else {
-				System.out.println("Thuật toán không hợp lệ");
-			}
-		}
-	
-	
-	//Cập nhật trạng thái của bàn cờ bằng cách đẩy trạng thái mới vào ngăn xếp state.
-	//Nếu ngăn xếp vượt quá dung lượng nhớ (memory), loại bỏ trạng thái cũ nhất.
-	// Bước 5 trong use case
+	// Cập nhật trạng thái của trò chơi
 	private void updateState(BoardState newState) {
 
 		state.push(newState);
@@ -133,20 +106,18 @@ public class Game {
 			state.remove(0);
 		}
 	}
-	
-	//Trả về trạng thái hiện tại của bàn cờ (đỉnh của ngăn xếp state).
+
+	// Trả về trạng thái hiện tại của trò chơi.
 	public BoardState getState() {
 		return state.peek();
 	}
-	
-	//Trả về người chơi hiện tại (lượt đi hiện tại).
+
+	// trả về lượt người chơi
 	public Player getTurn() {
 		return state.peek().getTurn();
 	}
-	
-	//Kiểm tra xem trò chơi đã kết thúc chưa bằng cách kiểm tra trạng thái đỉnh của ngăn xếp state.
-	//Cập nhật biến cờ humanWon dựa trên kết quả trò chơi.
-	//Bước 6: trong use case
+
+	// Kiểm tra xem trò chơi đã kết thúc chưa và trả về kết quả.
 	public boolean isGameOver() {
 
 		boolean isOver = state.peek().isGameOver();
@@ -158,8 +129,7 @@ public class Game {
 		return isOver;
 	}
 
-	//Trả về thông báo kết thúc trò chơi, bao gồm kết quả thắng hoặc thua của người chơi.
-	// Bước 7: trong use case
+	// Trả về kết quả của trò chơi.
 	public String getGameOverMessage() {
 		String result = "Trò chơi kết thúc.";
 		if (humanWon == true) {
@@ -169,9 +139,7 @@ public class Game {
 		}
 		return result;
 	}
-	
-	//Quay lại một hoặc nhiều trạng thái trước đó để thực hiện undo nước đi.
-	//Loại bỏ trạng thái khỏi ngăn xếp cho đến khi đến lượt người chơi.
+
 	public void undo() {
 
 		if (state.size() > 2) {
--- Settings.java
+++ Settings.java
@@ -1,38 +1,13 @@
-package main.gui;
-
-import main.game.Player;
+package main.game;
 
 public class Settings {
-
-	// cấu hình và cài đặt chung cho trò chơi
+	// cài đặt cấu hình cho trò chơi
+	public static boolean FORCETAKES = true;
 	
-	public static Colour AIcolour = Colour.BLACK; 
-    
-	  
-    public static boolean helpMode = true;
-    public static boolean hintMode = false;
-    public static boolean dragDrop = false;
-    
-    public static int squareSize = 80;
- 
-    public static int checkerWidth = 5*squareSize/6;
-    public static int checkerHeight = 5*squareSize/6;
- 
-    public static int ghostButtonWidth = 30*squareSize/29;
-    public static int ghostButtonHeight = 5*squareSize/6;
 
- //trả về màu sắc của người chơi dựa trên đối tượng Player.
-    public static Colour getColour(Player player){
-        Colour result = null;
-        if (player == Player.AI){
-            result = Settings.AIcolour;
-        }
-        else if (player == Player.HUMAN){
-            result = Settings.AIcolour.getOpposite();
-        }
-        if(result == null){
-            throw new RuntimeException("Người chơi không có quân cờ.");
-        }
-        return result;
-    }
+	public static Player FIRSTMOVE = Player.HUMAN;
+
+	
+	public static final int UNDO_MEMORY = 20;
+
 }
